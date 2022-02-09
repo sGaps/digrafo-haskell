cd65d5c8896a77a081c2e6bbd319a77110937321
sGaps <sgaps01@gmail.com>
2022-02-08 21:28:15 -0400

    Added vertex and edge methods
    But their names are in spanish.
    (`vertices` and `arcos`)

--- src/Digrafo.hs
+++ src/Digrafo.hs
@@ -12,14 +12,51 @@ module Digrafo (
     topologicalSort)
 where
 
+-- | TODO: Delete later
+grafo1 = (G [1..4] suc)
+    where suc 1 = [2,3]
+          suc 2 = [4]
+          suc 3 = [4]
+          suc 4 = []
 
 data Digrafo v = G [v] (v -> [v])
 
 vertices :: Digrafo v -> [v]
-vertices = undefined
+vertices (G vs _) = vs
+
+duplicate a = (a,a)
+
+-- utility function
+trans (G _ t) = t
+
+
+-- utility function
+runTransition :: (a -> b) -> [a] -> [(a, b)]
+runTransition transition = fmap (fmap transition . duplicate)
+
+
 
 arcos :: Digrafo v -> [(v, v)]
-arcos = undefined
+arcos g = mergePairs arcsByRoot
+    where arcsByRoot = runTransition (trans g) (vertices g)
+          mergePairs = concat . fmap (\(root,nexts) -> fmap ((,) root) nexts)
+          
+    -- what to do?
+    --  for every vertex, find the successor
+    --  so, we can have something like this:
+    --      1 -> [2,3]
+    --          implies:
+    --              (1,2), (2,3)
+    --      2 -> [4]
+    --          implies:
+    --              (2,4)
+    --     ...
+    --
+    --     so, we can construct this with the following function:
+    --     fmap (sucessor . duplicate) . vertices
+    --      -> 
+    --
+    --     fmap (\x) [1..4]
 
 nVertices :: Digrafo v -> Int
 nVertices = undefined
